87159e092a17fab4ad40ec268736eaba0d761f22
Yacine Habbak <151452334+Gayuura@users.noreply.github.com>
2024-07-04 15:17:42 +0200

    Ajout capture d'ecran pour GitHub

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -4,12 +4,12 @@
 
     <div class="container-fluid">
         <h1>Portail des Données de l'Enseignement Supérieur</h1>
-        <h3>Explorez les données des établissements,<br>les statistiques sur les étudiants,<br>et les taux d'insertion professionnelle<br>dans les départements français</h3>
+        <h3>Explorez les données des établissements,<br>les statistiques sur les étudiants,<br>et les taux d'insertion professionnelle<br>dans les académies françaises</h3>
         <img src="{{ asset('images/LeSaviezVous_1.png') }}" class="LeSaviezVous_1" alt="LeSaviezVous_1">
         <img src="{{ asset('images/LeSaviezVous_2.png') }}" class="LeSaviezVous_2" alt="LeSaviezVous_2">
         @include('include.carteAcad')
 
-        <p class="texte_explication">Cliquez sur un département pour accéder<br>aux informations détaillées sur les universités,<br>les étudiants, les ressources humaines<br>et les taux d'insertion professionnelle dans <br> le département séléctionné.</p>
+        <p class="texte_explication">Cliquez sur une académie pour accéder<br>aux informations détaillées sur les etablissements,<br>les étudiants, les ressources humaines<br>et les taux d'insertion professionnelle dans <br> l'académie séléctionnée.</p>
     
 
         <div class="d-flex justify-content-center position-bottom">
